c0b2c60223e7b5e3776ce5d64eaabecd4014c2ce
rafeedmohammad11 <rafeed.cse@gmail.com>
2025-08-05 00:49:17 +0600

    typo fixed

--- src/api/productService.jsx
+++ src/api/productService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://tailors-stitch.com'; // Your backend base URL
+const API_BASE_URL = 'https://tailors-stitch.com'; // Your backend base URL
 
 // Named export for the update function
 export const updateProduct = async (id, updatedProductData) => {
